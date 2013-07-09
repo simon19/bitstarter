b7b9d19d1592729db61c63d38022aa8fc386c55e
Simon Soet <simon.soet@outlook.com>
2013-07-09 19:08:15 +0000

    Updated

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var fs = require ('fs');
     var buf = new Buffer (fs.readFileSync('index.html', 'utf-8'));
-    Response.send(buf.toString());
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
